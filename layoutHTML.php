4ebb53c958b3e8f23908145771d75a03888c4c44
chau.le <chau.le@codegym.vn>
2019-10-16 14:03:51 +0700

    DemoPHP-init

---
+++ layoutHTML.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="container">
+    <header><?php include "header.php";  ?></header>
+    <nav><?php include "navigator.php";   ?></nav>
+    <article><?php include "content.php";?></article>
+    <footer><?php include "footer.php"; ?></footer>
+</div>
+
+
+</body>
+</html>
---
+++ navigator.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="#">Trang chủ</a></li>
+    <li><a href="#">Tin tức</a></li>
+    <li><a href="#">Thể thao</a></li>
+    <li><a href="#">Liên hệ</a></li>
+</ul>
